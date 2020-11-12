113bc25bc0b0bc907a90da54b288a874f9d861a3
Priyanka Thakan <priyankawu@yahoo.com>
2020-11-12 14:33:13 -0800

    Boolean Operators

--- Basic JavaScript Projects/Project4_dictionaries/JS/main.js
+++ Basic JavaScript Projects/Project4_dictionaries/JS/main.js
@@ -6,9 +6,26 @@ function my_Dictionary(){
         Color:"Red", 
         Breed:"Labrador", 
         Age: 5, 
-        Sound: "Bark!";
-
+        Sound: "Bark!"
     };
-    //delete Animal.Sound; //word operator
     document.getElementById("Dictionary").innerHTML = Animal.Sound;
 }
+
+//boolean isNaN("")
+    function my_Test(){
+        document.getElementById("Test").innerHTML=isNaN("hello");
+    }
+//why is this not working?
+    function my_Test1(){
+        document.getElementById("Test1").innerHTML=isNaN("007");
+    }
+
+    document.write(2E309); //positive infinity
+    document.write(3E309); //negative infinity
+    //delete Animal.Sound; //word operator
+    
+    //document.write(typeof 5);
+    //document.write(typeof "hello");
+
+   
+   
---
+++ Basic JavaScript Projects/Project5_comparisons_type_coercion/JS/main.js
@@ -0,0 +1,47 @@
+
+
+//boolean isNaN("")
+    function my_Test(){
+        //document.getElementById("Test").innerHTML=isNaN("hello");
+    }
+//why is this not working?
+    function my_Test1(){
+        //document.getElementById("Test1").innerHTML=isNaN("007");
+    }
+
+    //document.write(2E309); //positive infinity
+    //document.write(3E309); //negative infinity
+    
+    
+    //document.write(typeof 5);
+    //document.write(typeof "hello");
+    document.write(10>2);
+    document.write(10<2);
+    //used to debug
+    console.log(2+2);
+    //type coercion
+    document.write("10"+5);
+    document.write("hello"+5);
+    console.log(2>100);
+    document.write(3==4);
+    document.write("yes"=="yes")
+    document.write("yes"===0);
+    document.write(5===5);
+    document.write("5"===5);
+    document.write(3===8);
+    document.write("   ");
+    document.write(3>1 && "yes"==="yes");
+    document.write(10==9 && 3>2);
+    document.write("   OR    ");
+    document.write("Priyanka"=="Thakan" || 10<20);
+    document.write(45<29 || 4<1);
+    document.write("  NOT operator ");
+    
+    function not_Function(){
+        document.getElementById("Not").innerHTML = !(20>10);
+    }
+
+    function not_Function1(){
+        document.getElementById("Not1").innerHTML = !(20>100);
+    }
+   
