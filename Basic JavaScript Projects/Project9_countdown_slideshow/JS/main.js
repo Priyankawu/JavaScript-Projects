d7feca690bdb2963baff31876eec3bcf7822703a
Priyanka Thakan <priyankawu@yahoo.com>
2020-11-15 06:30:46 -0800

    space before active

--- Basic JavaScript Projects/Project9_countdown_slideshow/JS/main.js
+++ Basic JavaScript Projects/Project9_countdown_slideshow/JS/main.js
@@ -35,5 +35,6 @@ function showSlides(n) {
     }
     slides[slideIndex-1].style.display = "block";
 
-    dots[slideIndex-1].className += "active";
+    dots[slideIndex-1].className += " active";
+    //dots[slideIndex-1].className = dots[slideIndex-1].className + active;
 }
