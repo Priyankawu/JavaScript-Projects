a67345d4d876db5e3b2eaefc9d9bbb7c384ca86e
Priyanka Thakan <priyankawu@yahoo.com>
2020-11-19 13:29:14 -0800

    Final Pizza Project

--- Basic JavaScript Projects/Pizza_Project/Pizza_JavaScript/Pizza.js
+++ Basic JavaScript Projects/Pizza_Project/Pizza_JavaScript/Pizza.js
@@ -1,20 +1,18 @@
+// This function calculates the price of the pizza chosen.
 function getReceipt() {
     var text1 = "<h3>You Ordered:</h3>";
     var sizeTotal = 0;
     var runningTotal = 0;
     //[input.size, input.size, input.size, input.size, input.size]
     var sizeArray = document.getElementsByClassName("size");
-    console.log(sizeArray.length);
     for (var i=0; i < sizeArray.length; i++) { //a , instead of a ; caused 25 minutes of debugging :(
-        console.log("i "+i);
-        console.log(" +++++ "+sizeArray[i].value);
+       
         if (sizeArray[i].checked) {
             var selectedSize = sizeArray[i].value;
-            console.log("****** "+selectedSize[i].value);
             text1 = text1 + selectedSize + "<br>"; //put br in quotes
          }
     }
-    // what if they select more than one?? 
+    // match the chosen pizza and set it's price. 
     if(selectedSize === "Personal Pizza") {
         sizeTotal = 6;
     }
@@ -31,6 +29,7 @@ function getReceipt() {
         sizeTotal = 16;
     }
     runningTotal = sizeTotal;
+
     console.log(selectedSize+ " =$" +sizeTotal+ ".00");
     console.log("size text1: "+text1);
     console.log("subtotal: $" +runningTotal+ ".00");
@@ -38,25 +37,31 @@ function getReceipt() {
 }
 
 //***********************************************************//
+// This function calculates the total number of toppings chosen and 
+ // displays the Total price of the pizza.
 function getTopping(runningTotal, text1) {
+    // this variable tells how much money for toppings
     var toppingTotal = 0;
     var selectedToppings = [];
-    var toppingArray = document.getElementsByName("topppings");
+    var toppingArray = document.getElementsByClassName("toppings");
+    
     for (var j=0; j<toppingArray.length; j++) {
         if(toppingArray[j].checked) {
             selectedToppings.push(toppingArray[j].value);
-            console.log("selected topping item: ("+toppinArray[j].value+")");
+            console.log("selected topping item: ("+toppingArray[j].value+")");
             text1 = text1+toppingArray[j].value + "<br>";
         }
     }
+    //number of toppings
     var toppingCount = selectedToppings.length;
+    
     if(toppingCount > 1) {
-        toppingTotal = (toppingTotal-1);    // only charge for >1 toppings
+        toppingTotal = (toppingCount-1);    // only charge for >1 toppings
     }
     else {
-        toppingTotal = 0;                   //one free topping?
+        toppingTotal = 0;                   //one free topping
     }
-
+    
     runningTotal = (runningTotal + toppingTotal);
     console.log("total selected topping items: "+toppingCount);
     console.log(toppingCount + " topping - 1 free topping = "+"$"+toppingTotal+".00");
@@ -65,3 +70,5 @@ function getTopping(runningTotal, text1) {
     document.getElementById("showText").innerHTML = text1;
     document.getElementById("totalPrice").innerHTML = "<h3>Total: <strong>$" + runningTotal+".00"+"</strong></h3>";
 }
+
+/*********************************** DONE! *********************************/
